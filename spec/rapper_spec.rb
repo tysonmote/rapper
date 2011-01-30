3ea615c4af1d1ebe53482a031710fbdccd0eb8bf
Tyson Tate <tyson@doloreslabs.com>
2011-01-30 02:41:20 -0800

    More useful spec order.

--- spec/rapper_spec.rb
+++ spec/rapper_spec.rb
@@ -1,5 +1,39 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
+describe Closure::Compiler do
+  it "shoud be available" do
+    closure = Closure::Compiler.new
+    closure.compile( "var x = 1; var y = 2;" ).should == "var x=1,y=2;\n"
+  end
+    
+    it "provides whitespace-only, simple, and advanced compression" do
+      # Taken from https://github.com/documentcloud/closure-compiler/blob/master/test/unit/test_closure_compiler.rb
+      
+      original = "window.hello = function(name) { return console.log('hello ' + name ); }; hello.squared = function(num) { return num * num; }; hello('world');"
+      compiled_whitespace = "window.hello=function(name){return console.log(\"hello \"+name)};hello.squared=function(num){return num*num};hello(\"world\");\n"
+      compiled_simple = "window.hello=function(a){return console.log(\"hello \"+a)};hello.squared=function(a){return a*a};hello(\"world\");\n"
+      compiled_advanced = "window.a=function(b){return console.log(\"hello \"+b)};hello.b=function(b){return b*b};hello(\"world\");\n"
+      
+      Closure::Compiler.new( :compilation_level => "WHITESPACE_ONLY" ).compile(original).should == compiled_whitespace
+      Closure::Compiler.new.compile(original).should == compiled_simple
+      Closure::Compiler.new( :compilation_level => "ADVANCED_OPTIMIZATIONS" ).compile(original).should == compiled_advanced
+    end
+end
+
+describe YUI::CSSCompressor do
+  # Originally from: https://github.com/rhulse/ruby-css-toolkit/blob/master/test/yui_compressor_test.rb
+  test_files = Dir[File.join( File.dirname( __FILE__ ), '/fixtures/yui_css/*.css' )]
+  test_files.each_with_index do |file, i|
+    test_css = File.read(file)
+    expected_css = File.read( file + '.min' )
+    test_name = File.basename( file, ".css" )
+    
+    it "passes #{test_name} test" do
+      YUI::CSSCompressor.compress( test_css ).should == expected_css
+    end
+  end
+end
+
 describe Rapper do
   describe "setup" do
     it "loads configuration and environment" do
@@ -49,39 +83,5 @@ describe Rapper do
   describe "compressing" do
     it "can be turned off"
     it "compresses, if configured to"
-  end
-end
-
-describe Closure::Compiler do
-  it "shoud be available" do
-    closure = Closure::Compiler.new
-    closure.compile( "var x = 1; var y = 2;" ).should == "var x=1,y=2;\n"
-  end
-    
-    it "provides whitespace-only, simple, and advanced compression" do
-      # Taken from https://github.com/documentcloud/closure-compiler/blob/master/test/unit/test_closure_compiler.rb
-      
-      original = "window.hello = function(name) { return console.log('hello ' + name ); }; hello.squared = function(num) { return num * num; }; hello('world');"
-      compiled_whitespace = "window.hello=function(name){return console.log(\"hello \"+name)};hello.squared=function(num){return num*num};hello(\"world\");\n"
-      compiled_simple = "window.hello=function(a){return console.log(\"hello \"+a)};hello.squared=function(a){return a*a};hello(\"world\");\n"
-      compiled_advanced = "window.a=function(b){return console.log(\"hello \"+b)};hello.b=function(b){return b*b};hello(\"world\");\n"
-      
-      Closure::Compiler.new( :compilation_level => "WHITESPACE_ONLY" ).compile(original).should == compiled_whitespace
-      Closure::Compiler.new.compile(original).should == compiled_simple
-      Closure::Compiler.new( :compilation_level => "ADVANCED_OPTIMIZATIONS" ).compile(original).should == compiled_advanced
-    end
-end
-
-describe YUI::CSSCompressor do
-  # Originally from: https://github.com/rhulse/ruby-css-toolkit/blob/master/test/yui_compressor_test.rb
-  test_files = Dir[File.join( File.dirname( __FILE__ ), '/fixtures/yui_css/*.css' )]
-  test_files.each_with_index do |file, i|
-    test_css = File.read(file)
-    expected_css = File.read( file + '.min' )
-    test_name = File.basename( file, ".css" )
-    
-    it "passes #{test_name} test" do
-      YUI::CSSCompressor.compress( test_css ).should == expected_css
-    end
   end
 end
