5c2654465b0816f6fb602b8dbfa2a5c99164fb95
Tyson Tate <tyson@doloreslabs.com>
2011-01-29 23:31:05 -0800

    join_files()

--- lib/rapper/utils.rb
+++ lib/rapper/utils.rb
@@ -17,5 +17,16 @@ module Rapper
       ].join( "/" )
     end
     
+    # Contatenate one or more files. Uses <code>cat</code>.
+    # 
+    # @param [Array<String>,String] source_files A  path or array of paths to
+    #   files to concatenate.
+    # 
+    # @param [String] destination_file Destination for concatenated output.
+    def join_files( source_files, destination_file )
+      source_files = Array( source_files ).uniq.join( " " )
+      system "cat #{source_files} > #{destination_file}"
+    end
+    
   end
 end
