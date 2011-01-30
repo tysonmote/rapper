e4c0f9815aedcfaca1f4f7cc15738e2f0713dd72
Tyson Tate <tyson@doloreslabs.com>
2011-01-29 23:09:54 -0800

    CSSCompressor singleton and better specs.

--- spec/rapper_spec.rb
+++ spec/rapper_spec.rb
@@ -48,3 +48,17 @@ describe Rapper do
     it "compresses, if configured to"
   end
 end
+
+describe YUI::CSSCompressor do
+  # Originally from: https://github.com/rhulse/ruby-css-toolkit/blob/master/test/yui_compressor_test.rb
+  test_files = Dir[File.join( File.dirname( __FILE__ ), '/fixtures/yui_css/*.css' )]
+  test_files.each_with_index do |file, i|
+    test_css = File.read(file)
+    expected_css = File.read( file + '.min' )
+    test_name = File.basename( file, ".css" )
+    
+    it "passes #{test_name} test" do
+      YUI::CSSCompressor.compress( test_css ).should == expected_css
+    end
+  end
+end
